a3507ef019608efe176f2ad364c41c0023e0e085
Valera1980 <gavrilow777@gmail.com>
2021-03-24 09:30:02 +0200

    in progress

--- scripts/src/components/HelloWorld.vue
+++ scripts/src/components/HelloWorld.vue
@@ -103,6 +103,8 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
